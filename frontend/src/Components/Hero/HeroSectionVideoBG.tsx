fecd2a56e571b08d059945a753e2e50d97ab91ac
neptura-online <abhisheksamriya2024@gmail.com>
2026-01-05 17:17:45 +0530

    performance fix

--- frontend/src/Components/Hero/HeroSectionVideoBG.tsx
+++ frontend/src/Components/Hero/HeroSectionVideoBG.tsx
@@ -7,7 +7,6 @@ import { motion, AnimatePresence } from "framer-motion";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import video from "../../../public/videos/bg.mp4";
-import cover from "../../../public/assets/cover.webp";
 
 const HeroSectionVideoBG = ({ setOpenForm, setId }: OpenFormProps) => {
   const [formData, setFormData] = useState({
@@ -192,17 +191,7 @@ const HeroSectionVideoBG = ({ setOpenForm, setId }: OpenFormProps) => {
           autoPlay
           loop
           playsInline
-          poster={cover}
-        ></video>
-        {/* <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="absolute inset-0 h-full w-full object-cover"
-          src="/videos/bg.mp4"
-          poster="/assets/cover.webp"
-        /> */}
+        />
 
         <div className="absolute inset-0 bg-black/75" />
         <div className="container mx-auto flex max-w-6xl flex-row items-center gap-12 ">

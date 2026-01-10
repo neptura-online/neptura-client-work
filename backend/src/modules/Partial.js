f21b5b759d6af2628290cbaeb3d363b574b01a69
neptura-online <abhisheksamriya2024@gmail.com>
2026-01-10 12:45:32 +0530

    updated

--- backend/src/modules/Partial.js
+++ backend/src/modules/Partial.js
@@ -4,7 +4,7 @@ const partialLeadSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String },
-    phone: { type: Number, required: true, unique: true },
+    phone: { type: String, required: true, unique: true },
     industry: { type: String },
     leadType: { type: String, default: "PartialLead" },
     message: { type: String },

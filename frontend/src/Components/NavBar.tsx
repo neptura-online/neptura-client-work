77f226b9c2d8f767a92f69974bbae962e310ad1a
neptura-online <abhisheksamriya2024@gmail.com>
2026-01-13 17:43:52 +0530

    navbar bg early appear

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -7,8 +7,8 @@ const NavBar = () => {
 
   useEffect(() => {
     const handler = () => {
-      if (window.scrollY >= 70) setNavBg(true);
-      if (window.scrollY < 70) setNavBg(false);
+      if (window.scrollY >= 40) setNavBg(true);
+      if (window.scrollY < 40) setNavBg(false);
     };
     window.addEventListener("scroll", handler);
     return () => window.removeEventListener("scroll", handler);

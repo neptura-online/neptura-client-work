13ffa7927fd2fb17e743a631874873cd9624b3db
neptura-online <abhisheksamriya2024@gmail.com>
2026-01-15 14:51:35 +0530

    env issue fix

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   try {
     const url = process.env.DB_URL;
     await mongoose.connect(url);
-    console.log("database connected successful");
+    console.log("database connected successfull");
   } catch (error) {
     console.log("database not connected", error);
   }

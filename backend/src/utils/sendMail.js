1f5d43178ddaad7a30df76ada95e2687ddd56f0e
neptura-online <abhisheksamriya2024@gmail.com>
2026-01-16 10:52:26 +0530

    email formate change

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -128,7 +128,7 @@ export const sendLeadMail = async ({ name, email }) => {
     to: email,
     subject: "Thanks for contacting us!",
     html: `
-      <h2>Hello ${name},</h2>
+      <h6>Hello ${name},</h6>
       <p>Thanks for reaching out to us.</p>
       <p>Our team will contact you shortly.</p>
       <br/>

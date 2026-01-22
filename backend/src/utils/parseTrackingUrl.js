e2c9ec1ca11556096cd5ba7f2ecdaa594fd796b2
abhisheksamriya <abhisheksamriya2004@gmail.com>
2026-01-22 10:02:30 +0530

    new util added

---
+++ backend/src/utils/parseTrackingUrl.js
@@ -0,0 +1,19 @@
+export const parseTrackingUrl = (url) => {
+  try {
+    const parsedUrl = new URL(url);
+    const params = parsedUrl.searchParams;
+
+    return {
+      utm_source: params.get("utm_source"),
+      utm_medium: params.get("utm_medium"),
+      utm_campaign: params.get("utm_campaign"),
+      utm_term: params.get("utm_term"),
+      utm_content: params.get("utm_content"),
+      gclid: params.get("gclid"),
+      adgroupid: params.get("adgroupid"),
+      fbclid: params.get("fbclid"),
+    };
+  } catch (err) {
+    return {};
+  }
+};

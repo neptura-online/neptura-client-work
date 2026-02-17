4c52697bd12ee4312c8613cf6930ed5ff8cf5b2c
abhisheksamriya <abhisheksamriya2004@gmail.com>
2026-02-17 16:36:21 +0530

    change

--- backend/src/utils/googleSheet.js
+++ backend/src/utils/googleSheet.js
@@ -1,14 +1,15 @@
 import { google } from "googleapis";
 import { formatDate } from "./formateDate.js";
 
-console.log(process.env.GOOGLE_PRIVATE_KEY);
-
 export const addLeadToSheet = async (lead) => {
   const auth = new google.auth.GoogleAuth({
     credentials: {
       project_id: process.env.GOOGLE_PROJECT_ID,
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      private_key: Buffer.from(
+        process.env.GOOGLE_PRIVATE_KEY,
+        "base64"
+      ).toString("utf8"),
     },
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });

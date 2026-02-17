5b6e94047092f78b5ab41792c9cc9c414ef88802
abhisheksamriya <abhisheksamriya2004@gmail.com>
2026-02-17 14:34:18 +0530

    fix vhange

--- backend/src/utils/googleSheet.js
+++ backend/src/utils/googleSheet.js
@@ -10,16 +10,12 @@ export const addLeadToSheet = async (lead) => {
   console.log(mapping);
   if (!mapping || !mapping.isActive) return;
 
-  const privateKey = crypto.createPrivateKey({
-    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    format: "pem",
-  });
-
-  const auth = new google.auth.JWT({
-    email: process.env.GOOGLE_CLIENT_EMAIL,
-    key: privateKey.export({ format: "pem", type: "pkcs8" }),
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
+  const auth = new google.auth.JWT(
+    process.env.GOOGLE_CLIENT_EMAIL,
+    null,
+    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    ["https://www.googleapis.com/auth/spreadsheets"]
+  );
 
   const sheets = google.sheets({ version: "v4", auth });
 

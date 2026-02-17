fca1e077ab2df0c6c126070872cbb550562e82ad
abhisheksamriya <abhisheksamriya2004@gmail.com>
2026-02-17 17:00:54 +0530

    changes done

--- backend/src/utils/googleSheet.js
+++ backend/src/utils/googleSheet.js
@@ -7,12 +7,16 @@ export const addLeadToSheet = async (lead) => {
     const mapping = await SheetMapping.findOne({ formID: lead.formID });
     if (!mapping || !mapping.isActive) return;
 
-    const auth = new google.auth.JWT(
-      process.env.GOOGLE_CLIENT_EMAIL,
-      null,
-      process.env.GOOGLE_PRIVATE_KEY,
-      ["https://www.googleapis.com/auth/spreadsheets"]
-    );
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: Buffer.from(
+          process.env.GOOGLE_PRIVATE_KEY,
+          "base64"
+        ).toString("utf8"),
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
     console.log(process.env.GOOGLE_CLIENT_EMAIL);
 
     const sheets = google.sheets({ version: "v4", auth });

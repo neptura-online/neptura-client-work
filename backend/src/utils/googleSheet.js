585808ef2ebaa1ae6bcdfe4aea112692b551f52e
abhisheksamriya <abhisheksamriya2004@gmail.com>
2026-02-17 15:32:35 +0530

    changes done

--- backend/src/utils/googleSheet.js
+++ backend/src/utils/googleSheet.js
@@ -3,38 +3,42 @@ import { SheetMapping } from "../modules/SheetMapping.js";
 import { formatDate } from "./formateDate.js";
 
 export const addLeadToSheet = async (lead) => {
-  try {
-    const mapping = await SheetMapping.findOne({ formID: lead.formID });
-    if (!mapping || !mapping.isActive) return;
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
 
-    const auth = new google.auth.JWT(
-      process.env.GOOGLE_CLIENT_EMAIL,
-      null,
-      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      ["https://www.googleapis.com/auth/spreadsheets"]
-    );
-    console.log(process.env.GOOGLE_CLIENT_EMAIL);
+  const sheets = google.sheets({ version: "v4", auth });
+  const time = formatDate(lead.createdAt);
 
-    const sheets = google.sheets({ version: "v4", auth });
-
-    const time = formatDate(lead.createdAt);
-    const row = mapping.fields.map((field) => {
-      if (field.leadField === "time") return time;
-      return lead[field.leadField] || "-";
-    });
-
-    const response = await sheets.spreadsheets.values.append({
-      spreadsheetId: mapping.spreadsheetId,
-      range: `${mapping.sheetName}!A1`,
-      valueInputOption: "RAW",
-      requestBody: { values: [row] },
-    });
-
-    console.log("Lead successfully added to sheet:", response.status);
-  } catch (error) {
-    console.error(
-      "CRITICAL ERROR in addLeadToSheet:",
-      error.response?.data || error.message
-    );
-  }
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: "1a9JNuD1IRVfeKcS1ZmqXVYTWd8LGD5mNukZOeHgPBbE",
+    range: "Sheet1!A1",
+    valueInputOption: "RAW",
+    requestBody: {
+      values: [
+        [
+          time,
+          lead.name,
+          lead.email,
+          lead.phone,
+          lead.industry,
+          lead.message,
+          lead.utm_source,
+          lead.utm_medium,
+          lead.utm_term,
+          lead.utm_campaign,
+          lead.utm_content,
+          lead.adgroupid,
+          lead.gclid,
+          lead.lpurl,
+          lead.formID,
+        ],
+      ],
+    },
+  });
 };

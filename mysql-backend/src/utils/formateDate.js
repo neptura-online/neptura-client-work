f1b0d8d4f13b07a09aafcdd7c9a8e284ded97892
abhisheksamriya <abhisheksamriya2004@gmail.com>
2026-02-18 14:24:33 +0530

    changes done

--- mysql-backend/src/utils/formateDate.js
+++ mysql-backend/src/utils/formateDate.js
@@ -1,29 +1,20 @@
-// export function formatDate(d) {
-//   const year = d.getFullYear();
-//   const month = String(d.getMonth() + 1).padStart(2, "0");
-//   const day = String(d.getDate()).padStart(2, "0");
-
-//   let hours = d.getHours();
-//   const minutes = String(d.getMinutes()).padStart(2, "0");
-//   const seconds = String(d.getSeconds()).padStart(2, "0");
-
-//   const ampm = hours >= 12 ? "pm" : "am";
-
-//   hours = hours % 12 || 12;
-
-//   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${ampm}`;
-// }
-
 export function formatDate(d) {
-  const year = d.getUTCFullYear();
-  const month = String(d.getUTCMonth() + 1).padStart(2, "0");
-  const day = String(d.getUTCDate()).padStart(2, "0");
+  const date = new Date(
+    new Date(d).toLocaleString("en-US", {
+      timeZone: "Asia/Kolkata",
+    })
+  );
 
-  let hours = d.getUTCHours();
-  const minutes = String(d.getUTCMinutes()).padStart(2, "0");
-  const seconds = String(d.getUTCSeconds()).padStart(2, "0");
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  let hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
 
   const ampm = hours >= 12 ? "pm" : "am";
+
   hours = hours % 12 || 12;
 
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${ampm}`;
